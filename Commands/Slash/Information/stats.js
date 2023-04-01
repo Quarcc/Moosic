b5f8d5950bca83861a310913a94b66b1abfd356f
Brandon <121778246+Quarcc@users.noreply.github.com>
2023-04-01 23:33:40 +0800

    Update stats.js

--- Commands/Slash/Information/stats.js
+++ Commands/Slash/Information/stats.js
@@ -46,7 +46,7 @@ module.exports = {
           .setTitle("__**Stats:**__")
           .setThumbnail(client.user.displayAvatarURL())
           .setDescription(
-            `> ** Made by [\` Kabir Jaipal \`](https://www.instagram.com/kabirjaipal_2004) **`
+            `> ** Made by [\` Brandon Wee \`](https://www.instagram.com/brandoonix) **`
           )
           .addFields([
             {
